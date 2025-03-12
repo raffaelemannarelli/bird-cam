c6c32e23b3f000b7fc0f95ab89a35c697306baf3
Raffaele Mannarelli <raffaelemannarelli@Raffaeles-MacBook-Pro.local>
2025-03-11 22:18:04 -0400

    added gpu acceleration with metal but still too slow dont wanna kill my laptop rn

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from ultralytics import YOLO
+import torch
 
 # Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo11n.pt")
+
+# Set the device (force MPS if available)
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+model.to(device)
 
 # Train the model
-results = model.train(data="../yolo_dataset/dataset.yaml", epochs=100, imgsz=640)
+results = model.train(data="../yolo_dataset/dataset.yaml", epochs=100, imgsz=640, device=device)
